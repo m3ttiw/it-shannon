82a1c95bd722671a7a395f845af700b39e81587b
mpatruno <admin@example.com>
2023-10-31 16:14:30 +0100

    init

---
+++ core/entropy.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+def entropy(X):
+    pass
+
+
+def entropy(X, Y, mode="|"):
+    if mode == "|":
+        pass
+    elif mode == ",":
+        pass
+    pass
+    
+
+def bayes_rule(X, Y):
+    pass
+
+def mutual_information(X, Y):
+    pass
